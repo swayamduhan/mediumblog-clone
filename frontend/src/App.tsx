b00a469de14e1d8bec2bb51b3a97f215b2b29ad5
Swayam Duhan <workplace.swayam@gmail.com>
2024-09-22 18:13:34 +0530

    minor changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { Blogs } from './pages/Blogs'
 import './App.css'
 import { Auth } from './pages/Auth'
 import { Publish } from './pages/Publish'
-import { Forgot } from './pages/Forgot'
 import { BlogPage } from './pages/BlogPage'
 import { Null } from './pages/Null'
 
@@ -16,7 +15,6 @@ function App() {
           {/* Null path is only for dev  */}
           <Route path='/' element={<Null />} />  
           <Route path='/auth' element={<Auth />}/>
-          <Route path='/auth/forgot' element={<Forgot />}/>
           <Route path='/blogs' element={<Blogs />}/>
           <Route path='/publish' element={<Publish />}/>
           <Route path='/blog/:id' element={<BlogPage />}/>
