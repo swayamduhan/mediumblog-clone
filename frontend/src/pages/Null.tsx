a7197455a9b1e1a1a10fe07c8d4ce3c9e27009e0
Swayam Duhan <workplace.swayam@gmail.com>
2024-09-22 18:25:11 +0530

    :P

--- frontend/src/pages/Null.tsx
+++ frontend/src/pages/Null.tsx
@@ -1,11 +1,12 @@
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom"
 
 export function Null(){
     const navigate = useNavigate();
-    navigate('/auth')
+    useEffect(()=>{
+        navigate('/auth')
+    }, [])
     return(
-        <div className="p-10">
-            <pre>Go to /auth</pre>
-        </div>
+        <></>
     )
 }
