c6e0c0298cfc288073841d3a56736cfc91a9a981
Swayam Duhan <workplace.swayam@gmail.com>
2024-09-22 18:18:46 +0530

    added automatic landing

--- frontend/src/pages/Null.tsx
+++ frontend/src/pages/Null.tsx
@@ -1,4 +1,8 @@
+import { useNavigate } from "react-router-dom"
+
 export function Null(){
+    const navigate = useNavigate();
+    navigate('/auth')
     return(
         <div className="p-10">
             <pre>Go to /auth</pre>
